1f53439cfd02344446ac7498179aa8ab00c1aa72
samidi27 <it22607232@my.sliit.lk>
2023-04-14 12:18:23 +0530

    EX02 done

--- Lab03 Ex02/Lab03 Ex02.cpp
+++ Lab03 Ex02/Lab03 Ex02.cpp
@@ -2,11 +2,38 @@
 //
 
 #include <iostream>
+#include<iomanip>
+using namespace std;
+
+void findMarks(float t_marks1, float t_marks2, float& t_CA1, float& t_CA2);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float marks1, marks2, CA1 = 0, CA2 = 0;
+
+	for (int i = 1; i <= 5; i++) 
+	{
+		cout << "Enter mark1 ";
+		cin >> marks1;
+
+		cout << "Enter mark2 ";
+		cin >> marks2;
+
+		findMarks(marks1, marks2, CA1, CA2);
+		cout << setprecision(2) << setiosflags(ios::fixed);
+		cout << "Student" << i << ":"<<endl<< "Marks1"<<":" << marks1 << "\t" << "Marks2" << ":"<< marks2 << "\t" << "CA1" << ":"  << CA1 << "\t" << "CA2" << ":" << CA2 << endl;
+	}
+	return 0;
 }
+
+void findMarks(float t_marks1, float t_marks2, float& t_CA1, float& t_CA2) 
+{
+
+	t_CA1 = t_marks1 * 20 /100;
+	t_CA2 = (t_marks2 * 30) / 100;
+}
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
